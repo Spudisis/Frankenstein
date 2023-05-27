d4d6779f82f8dcb16e39717fc8c5bf5fcb3d6b7a
Spudisis <alekasisbb@gmail.com>
2023-05-27 14:37:35 +0300

    httpAgent templates

---
+++ src/http/agent/Templates.agent.ts
@@ -0,0 +1,37 @@
+import { BasicAgent } from "./Basic";
+
+class TemplatesAgent extends BasicAgent {
+  constructor() {
+    super(process.env.REACT_APP_URL_BACK + "templates", {
+      withCredentials: true,
+    });
+  }
+  async getTemplates(page = 1, limit = 10, type: string) {
+    const { data } = await this._http.get<any>(
+      `/getType?p=${page}&l=${limit}&type=${type}`
+    );
+    return data;
+  }
+
+  async getTemplatesUser(page = 1, limit = 10, type?: string) {
+    const { data } = await this._http.get<any>(
+      `/getUser?p=${page}&l=${limit}&type=${type}`
+    );
+    return data;
+  }
+
+  async createTemplate(body: any) {
+    //{ name, privateStatus, type, layout, miniature }
+    const { data } = await this._http.post(`/create`, body);
+    return data;
+  }
+  async update(body: any) {
+    const { data } = await this._http.patch(`/update`, body);
+    return data;
+  }
+  async deleteTemplate(id: number) {
+    const { data } = await this._http.delete(`/create&id=${id}`);
+    return data;
+  }
+}
+export const Templates = new TemplatesAgent();
